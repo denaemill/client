504b46a11a2174ddd47fd2333b26a3b39b4d803e
denaemill <114959977+denaemill@users.noreply.github.com>
2023-02-05 16:38:54 -0500

    Update client.py

--- client.py
+++ client.py
@@ -14,81 +14,89 @@ try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 except socket.error:
-    print("ERROR: Socket not created")
+    print("ERROR: ()Socket not created")
     sys.exit(1)
 
 try:
     sock.settimeout(10)  # Setting timeouts for the code
     port = int(port)
-    sock.connect((host, port))
+    if port < 0 and port > 65535:
+        try:
+            sock.connect((host, port))
+        except socket.error:
+            print("ERROR: ()port must be 0-65535")
+            sys.exit(1)
+    else:
+        sock.connect((host, port))
 
 except socket.gaierror:
-    print("ERROR: Address-related error connecting to server")
+    print("ERROR: ()Address-related error connecting to server")
     sys.exit(1)
 
 except socket.error:
-    print("ERROR: No connection established")
+    print("ERROR: ()No connection established")
     sys.exit(1)
 
 
 #Getting two commands from the connection
-i = 0
-msg = b""
-while i < 2:
+try:
+    i = 0
+    msg = b""
+    while i < 2:
 
-    #Recieves commands from the server bit by bit
-    while True:
-
-        m = sock.recv(1)
-
-        msg += m
-
-        if msg.find(b"\n") != -1:
-            break
-
-        # Connection is closed by server
-        elif len(m) <= 0:
-            break
-
-
-    #Checks if there was a command recorded
-    if len(msg) > 0:
-
-        if msg.find(b"accio\r\n") != -1:
-
-            if i == 0:
-                i += 1
-                msg = b""
-                sock.send(b"confirm-accio\r\n")
-
-            elif i == 1:
-                i += 1
-                msg = b""
-                sock.send(b"confirm-accio-again\r\n\r\n")
-
-        # Continues to append to the mg string until
-        # ... it matches the specified command
-        elif len(msg) < len("accio\r\n"):
-            continue
-
-
-
-#Transfer the specified file
-if i == 5:
-
-    #Opens file in binary
-    with open(file, "rb") as f:
-
+        # Recieves commands from the server bit by bit
         while True:
-            fileIn = f.read(BUFFER_SIZE)
 
-            if not fileIn:
+            m = sock.recv(1)
+
+            msg += m
+
+            if msg.find(b"\n") != -1:
                 break
 
-            byteSize = sock.send(fileIn)
+            # Connection is closed by server
+            elif len(m) <= 0:
+                break
 
+        # Checks if there was a command recorded
+        if len(msg) > 0:
 
+            if msg.find(b"accio\r\n") != -1:
 
+                if i == 0:
+                    i += 1
+                    msg = b""
+                    sock.send(b"confirm-accio\r\n")
 
-#Ending the program
-sock.close()
+                elif i == 1:
+                    i += 1
+                    msg = b""
+                    sock.send(b"confirm-accio-again\r\n\r\n")
+
+            # Continues to append to the mg string until
+            # ... it matches the specified command
+            elif len(msg) < len("accio\r\n"):
+                continue
+
+    # Transfer the specified file
+    if i == 2:
+
+        # Opens file in binary
+        with open(file, "rb") as f:
+
+            byteSize = 0
+            while True:
+                fileIn = f.read(BUFFER_SIZE - byteSize)
+
+                if not fileIn:
+                    break
+
+                temp = sock.send(fileIn)
+                byteSize += temp
+
+    # Ending the program
+    sock.close()
+
+except socket.error:
+    print("ERROR: ()Connection is not longer available.")
+    sys.exit(1)
